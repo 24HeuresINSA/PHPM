8fa6e5e5587313955f7778774f2138e84e6ba5ee
Laurent BILLON <laurent.billonc@gmail.com>
2011-12-28 17:04:19 +0100

    generated Creneau entity

---
+++ src/PHPM/Bundle/Entity/Creneau.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace PHPM\Bundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * PHPM\Bundle\Entity\Creneau
+ */
+class Creneau
+{
+    /**
+     * @var integer $id
+     */
+    private $id;
+
+    /**
+     * @var datetime $debut
+     */
+    private $debut;
+
+    /**
+     * @var datetime $fin
+     */
+    private $fin;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set debut
+     *
+     * @param datetime $debut
+     */
+    public function setDebut($debut)
+    {
+        $this->debut = $debut;
+    }
+
+    /**
+     * Get debut
+     *
+     * @return datetime 
+     */
+    public function getDebut()
+    {
+        return $this->debut;
+    }
+
+    /**
+     * Set fin
+     *
+     * @param datetime $fin
+     */
+    public function setFin($fin)
+    {
+        $this->fin = $fin;
+    }
+
+    /**
+     * Get fin
+     *
+     * @return datetime 
+     */
+    public function getFin()
+    {
+        return $this->fin;
+    }
+}
