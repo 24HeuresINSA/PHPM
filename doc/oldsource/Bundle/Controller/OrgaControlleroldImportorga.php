df71175a701b4ce1527c0995053809fd2c98ce70
Sylvain <sylvain.bourgin@gmail.com>
2011-12-29 20:42:23 +0100

    Et c'est la fin de l'import orga. Let's try again !

--- doc/oldsource/Bundle/Controller/OrgaControlleroldImportorga.php
+++ doc/oldsource/Bundle/Controller/OrgaControlleroldImportorga.php
@@ -225,14 +225,180 @@ class OrgaController extends Controller
      * Import Orgas from website.
      *
      * @Route("/import", name="orga_import")
-     * @Template
+     * @Template()
      */
 	public function importAction()	
 	{
+	
+        $em = $this->getDoctrine()->getEntityManager();
 
+        $entitiesOrga = $em->getRepository('PHPMBundle:Orga')->findAll();	
+	
+			$url = "inscriptionOrgas.json";			
+			$json = file_get_contents($url);
+			
+			$listeOrgaArray = json_decode($json,TRUE);
+			
+			
+
+			
+			
+			foreach($listeOrgaArray as $case => $inscriptionOrga)
+				{
+					$inscriptionOrga['nom']=strtoupper($inscriptionOrga['nom']);
+					$inscriptionOrga['prenom']=strtoupper($inscriptionOrga['prenom']);	
+					
+					$orgaNonTrouve=TRUE;
+					$i = 0;
+					foreach ($entitiesOrga as $key) 
+					{
+						$nomOrgaBDD = $entitiesOrga[$i]->getnom();    
+						$prenomOrgaBDD = $entitiesOrga[$i]->getprenom();
+						$telephoneOrgaBDD = $entitiesOrga[$i]->gettelephone();					
+						$emailOrgaBDD = $entitiesOrga[$i]->getemail();
+						$surnomOrgaBDD = $entitiesOrga[$i]->getsurnom();
+						$departementOrgaBDD = $entitiesOrga[$i]->getdepartement();
+						$commentaireOrgaBDD = $entitiesOrga[$i]->getcommentaire();
+						$permisOrgaBDD = $entitiesOrga[$i]->getpermis();
+						$dateDeNaissanceOrgaBDD = $entitiesOrga[$i]->getDateDeNaissance();
+						
+						
+						
+						if ($inscriptionOrga['nom'] == $nomOrgaBDD AND $inscriptionOrga['prenom'] == $prenomOrgaBDD
+							AND $inscriptionOrga['telephone'] == $telephoneOrgaBDD)
+						{
+							$orgaNonTrouve=FALSE;
+						}
+						
+						if ($inscriptionOrga['nom'] == $nomOrgaBDD AND $inscriptionOrga['prenom'] == $prenomOrgaBDD
+						AND $inscriptionOrga['telephone'] == $telephoneOrgaBDD AND $inscriptionOrga['email'] == $emailOrgaBDD
+						AND $inscriptionOrga['surnom'] == $surnomOrgaBDD AND $inscriptionOrga['departement'] == $departementOrgaBDD
+						AND $inscriptionOrga['commentaire'] == $commentaireOrgaBDD AND $inscriptionOrga['permis'] == $permisOrgaBDD)
+							{
+					//			echo $dateDeNaissanceOrgaBDD;
+								echo 'orga deja present a lidentique dans la bdd';
+							}
+
+						// rajouter les permis et date de naissance
+						
+						
+						$i++;
+					}
+
+
+
+					if ($orgaNonTrouve)    // si l'orga nexiste pas on lajoute
+						{
+						echo $inscriptionOrga['prenom'];
+						echo 'orga a rajouter';
+						echo "<p>";		
+						
+						$confiance = $entitiesOrga = $em->getRepository('PHPMBundle:Confiance')->findOneById(1);  // pour récupérer confiance
+						
+						$entity  = new orga();
+						$entity->setNom($inscriptionOrga['nom']);
+						$entity->setPrenom($inscriptionOrga['prenom']);
+						$entity->setConfiance($confiance);
+						$entity->settelephone($inscriptionOrga['telephone']);
+						$entity->setemail($inscriptionOrga['email']);
+						$entity->setdepartement($inscriptionOrga['departement']);
+						$entity->setcommentaire($inscriptionOrga['commentaire']);
+						$entity->setpermis($inscriptionOrga['permis']);
+						$entity->setDateDeNaissance(new \DateTime($inscriptionOrga['dateDeNaissance']));
+						$entity->setSurnom($inscriptionOrga['surnom']);				
+						$em->persist($entity);
+	            		
+	            		
+						// ajout des disponibilite
+						
+
+						$em->flush();	
+						$idOrgaAjoute= $em->getRepository('PHPMBundle:Orga')->findOneByTelephone($inscriptionOrga['telephone']);	
+
+						
+						
+						foreach($inscriptionOrga['disponibilites'] as $dispoAAjoute)
+							{
+								$entitydisponibilite = new disponibilite();						
+								$entitydisponibilite->setOrga($idOrgaAjoute);
+								$debutdispo = date ('y-m-d', $dispoAAjoute[0]);
+								$entitydisponibilite->setDebut(new \DateTime("20$debutdispo"));
+								$findispo = date ('y-m-d', $dispoAAjoute[1]);
+								$entitydisponibilite->setFin(new \DateTime("20$findispo"));
+								$em->persist($entitydisponibilite);
+								$em->flush();							
+							}
+
+						
+						
+						
+						
+	            		
+	            	//	$em->flush();
+							
+						
+																
+						}
+	
+
+
+					
+					
+					
+	
+	
+	
+	
+					}
+						
+					/*
+					echo ("<pre>");
+					print_r($entitiesOrga);
+					echo("</pre>");
+					
+					//echo $Nom;
+					echo ("<pre>");
+					print_r($inscriptionOrga);
+					echo("</pre>");
+					 */ 
+					
+		//		}
+			
+
+
+	
+			
+			
+			echo "<p>";
+			echo "plouf";
+			/*
+			echo ("<pre>");
+			var_dump($listeOrgaArray);
+			echo("</pre>");
+				*/
+				
+				
+				
+			
+			/*	
+			$plagehorraire = array ("1325083264", "1325083265");	
+				
+			$disponibilites = array ($plagehorraire,$plagehorraire, $plagehorraire);
+							
+        	$orga = array("id"=>1, "nom"=>"Bourgin", "prenom" => "Sylvain", "telephone" => "0685178329",
+        	"email" => "patate@gmail.com", "dateDeNaissance" => "2012-01-01", "departement" => "IF", 
+        	"commentaire" => "Le charisme c'est au BDE qu'on le trouve", "permisB" => "true", "permisB2ans" => "true",
+        	"disponibilites" => $disponibilites);
+			$orgas = array($orga,$orga,$orga,$orga);
+			exit(print_r(json_encode($orgas)));
+			*/
+	
+  
      	
+     	
+     	return array();
 		
-		return array();
+		
 	}
 	
 }
