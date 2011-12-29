184c61d1c412f32ead5d80484f76c990157121cd
Cédric Robert <cedrink.robert@gmail.com>
2011-12-30 00:51:22 +0100

    Added importId to Tache

--- src/PHPM/Bundle/Entity/Tache.php
+++ src/PHPM/Bundle/Entity/Tache.php
@@ -22,6 +22,13 @@ class Tache
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
+    
+    /**
+    * @var integer $importId
+    *
+    * @ORM\Column(name="importid", type="integer")
+    */
+    private $importId;
 
     /**
      * @var string $nom
@@ -80,16 +87,7 @@ class Tache
     */
     protected $plagesHoraire;
 
-    /**
-    * Set id
-    *
-    * @param string $id
-    */
-    public function setId($id)
-    {
-    	$this->id = $id;
-    }
-    
+
     
     /**
      * Get id
