91b681f8c2b4c9cc84c320a92ca53ebc2b18f29f
Sylvain <sylvain.bourgin@gmail.com>
2011-12-30 20:48:49 +0100

    Quart heure validator in progress

--- src/PHPM/Bundle/Validator/QuartHeureValidator.php
+++ src/PHPM/Bundle/Validator/QuartHeureValidator.php
@@ -1,1 +1,27 @@
-// ici sera le validateur des 1/4 d'heures 
+<?php
+
+namespace Symfony\Component\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+*
+*/
+class quartHeuerValidator extends ConstraintValidator
+{
+    /**
+* Configures the form field and options
+*/
+    public function isValid($creneauDebut, $creneauFin)
+    {
+		if (($creneauDebut % 900)==0 AND ($creneauDebut % 900) == 0)
+		{
+			return TRUE;
+		}
+		else 
+		{
+			return FALSE;	
+		}
+    }
+}
+
