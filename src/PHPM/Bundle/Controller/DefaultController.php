efff64cec0676cd61e470e266b8f761259ebf6d8
Laurent BILLON <laurent.billonc@gmail.com>
2012-01-04 18:05:06 +0100

    Security + initialconfig

--- src/PHPM/Bundle/Controller/DefaultController.php
+++ src/PHPM/Bundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace PHPM\Bundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use PHPM\Bundle\Entity\Config;
 
 class DefaultController extends Controller
 {
@@ -14,6 +15,14 @@ class DefaultController extends Controller
      */
     public function homeAction()
     {
+    	$em = $this->getDoctrine()->getEntityManager();
+    	$pref = $em->getRepository('PHPMBundle:Config')->findOneByField('phpm.config.initiale');
+    	
+    	
+    	if (!$pref)
+    	return $this->redirect($this->generateUrl('config_initiale'));
+    	
+    	
         return array();
     }
 }
