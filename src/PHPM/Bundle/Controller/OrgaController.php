118acf8e8e33e0e324d9aea25730c5cd6b44b6f3
Sylvain <sylvain.bourgin@gmail.com>
2012-01-07 11:29:17 +0100

    Validation Orga dans contrôler orga : done

--- src/PHPM/Bundle/Controller/OrgaController.php
+++ src/PHPM/Bundle/Controller/OrgaController.php
@@ -229,39 +229,46 @@ class OrgaController extends Controller
 		
         
         $request = $this->get('request')->request;
-        
-        /*
-         * chose à faire
-         * récupérer l'Arras associatif concernant les orga et traiter :
-if supprimer : alors supprimer l'orga et ses créneaux
-if en attente : ne rien faire
-if valider : laisser ce qu'il y avait avant.
-         * 
-         */
-
-            
-            
-        
-        
+      
 		$em = $this->getDoctrine()->getEntityManager();
 				
-                //var_dump($valeurs);
-		if ($this->get('request')->getMethod() == 'POST') {
-           
+		if ($this->get('request')->getMethod() == 'POST') 
+		{
 
         $data = $request->all();
-        var_dump($data);
-        exit();
-   		for ($i = 0; $i < count($_POST["Orga_Valid"]); $i++)
-			{
-				$orgaValide = $em->getRepository('PHPMBundle:Orga')->findOneById($_POST["Orga_Valid"][$i]);	
-				$orgaValide->setStatut(1);
-				$em->persist($orgaValide);
-				$em->flush();	
+     	    foreach ($data as $idOrgaATraiter => $codeFormulaire) // code formulaire : 0 rien à faire, 1 validé, 2 supprimé
+       
+            {
+               echo $idOrgaATraiter;         
+			   echo $codeFormulaire;
+	                  
+               if ($codeFormulaire==1)
+               {
+                  $orgaValide = $em->getRepository('PHPMBundle:Orga')->findOneById($idOrgaATraiter);
+                  $orgaValide->setStatut(1); // validation de l'orga
+                $em->persist($orgaValide);
+                $em->flush();   
+               } 
+               
+               if ($codeFormulaire==2)
+               
+               {
+                   $orgaASupprimer = $em->getRepository('PHPMBundle:Orga')->findOneById($idOrgaATraiter);
+                   
+                   $dispoADesafecter = $em->getRepository('PHPMBundle:Disponibilite')->findByOrga($idOrgaATraiter);
+                   
+                   foreach ($dispoADesafecter as $dispoAVirer) 
+                     {
+                        $em->remove($dispoAVirer); // suppression des dispos de l'orga
+                     }                   
+                   $em->remove($orgaASupprimer);  
+                   $em->flush();  
+               } 
+           
+				
 			}
 		}
 
-	
 		$orgaAValider = $em->getRepository('PHPMBundle:Orga')->findByStatut(0);
 		
 		$listeOrgaARetourne = array();
