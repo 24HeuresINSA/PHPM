af17e6eadd9b9241aac18af72931627f46283cfe
Romaric Drigon <romaric.drigon@gmail.com>
2012-01-07 23:25:39 +0100

    Removed hack code for null creneau

--- src/PHPM/Bundle/Resources/public/js/affectation/model/OrgaModel.js
+++ src/PHPM/Bundle/Resources/public/js/affectation/model/OrgaModel.js
@@ -75,22 +75,15 @@ OrgaModel.prototype = {
 			
 			// disponibilités, cela devient physique
 			for (var _iDispo in this.data[_iOrga]['disponibilites']) {
-				// TODO : hack à virer
-				if (this.data[_iOrga]['disponibilites'][_iDispo] === null) {
-					console.error(this.data[_iOrga]['disponibilites'], 'On a vu un null !');
-					delete this.data[_iOrga]['disponibilites'][_iDispo];
-				} else {
-					
-					// créneaux, encore un niveau
-					for (var _iCreneau in this.data[_iOrga]['disponibilites'][_iDispo]['creneaux']) {
-						this.data[_iOrga]['disponibilites'][_iDispo]['creneaux'][_iCreneau]['debut'] = new Date(this.data[_iOrga]['disponibilites'][_iDispo]['creneaux'][_iCreneau]['debut']);
-						this.data[_iOrga]['disponibilites'][_iDispo]['creneaux'][_iCreneau]['fin'] = new Date(this.data[_iOrga]['disponibilites'][_iDispo]['creneaux'][_iCreneau]['fin']);
-					}
-					
-					this.data[_iOrga]['disponibilites'][_iDispo]['debut'] = new Date(this.data[_iOrga]['disponibilites'][_iDispo]['debut']);
-					this.data[_iOrga]['disponibilites'][_iDispo]['fin'] = new Date(this.data[_iOrga]['disponibilites'][_iDispo]['fin']);
+				// créneaux, encore un niveau
+				for (var _iCreneau in this.data[_iOrga]['disponibilites'][_iDispo]['creneaux']) {
+					this.data[_iOrga]['disponibilites'][_iDispo]['creneaux'][_iCreneau]['debut'] = new Date(this.data[_iOrga]['disponibilites'][_iDispo]['creneaux'][_iCreneau]['debut']);
+					this.data[_iOrga]['disponibilites'][_iDispo]['creneaux'][_iCreneau]['fin'] = new Date(this.data[_iOrga]['disponibilites'][_iDispo]['creneaux'][_iCreneau]['fin']);
 				}
-			}
+				
+				this.data[_iOrga]['disponibilites'][_iDispo]['debut'] = new Date(this.data[_iOrga]['disponibilites'][_iDispo]['debut']);
+				this.data[_iOrga]['disponibilites'][_iDispo]['fin'] = new Date(this.data[_iOrga]['disponibilites'][_iDispo]['fin']);
+				}
 		
 			_orgas[_iOrga] = _orga;
 		}
