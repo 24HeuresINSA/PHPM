543c8290699e70a7e9cd4d2456153de358c7093d
Laurent BILLON <laurent.billonc@gmail.com>
2012-02-26 03:11:33 +0100

    Added app config sql file

---
+++ doc/config.sql
@@ -0,0 +1,8 @@
+INSERT INTO `Config` (`id`, `field`, `label`, `value`) VALUES
+(1, 'manifestation.plages', 'Plages de la manifestation', '{"1":{"nom":"Prémanif","debut":"2012-05-16 00:00","fin":"2012-05-24 00:00"},"2":{"nom":"Manif","debut":"2012-05-24 00:00","fin":"2012-05-27 00:00"},"3":{"nom":"Postmanif","debut":"2012-05-28 00:00","fin":"2012-06-01 00:00"}}'),
+(2, 'manifestation.organisation.nom', 'Nom de l''organisation', '24 Heures de l''INSA'),
+(3, 'phpm.config.initiale', 'PHPlanningMaker configuré', '1'),
+(4, 'server.baseurl', 'URL du serveur', 'localhost'),
+(5, 'manifestation.nom', 'Nom de la manifestation', '24 Heures de l''INSA 38e'),
+(6, 'manifestation.orga.responsableconfiancemin', 'Confiance minimale pour être responsable d''une tâche', '500'),
+(7, 'manifestation.permis.libelles', 'Libellés des permis', '{"0": "Pas de permis","1": "Permis - de 2 ans","2": "Permis de + de 2 ans" }');
