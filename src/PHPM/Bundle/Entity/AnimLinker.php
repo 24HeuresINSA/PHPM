81605f5d560a1c9573848d3209a5511c5f635e2b
Romaric Drigon <romaric.drigon@gmail.com>
2012-03-23 22:14:08 +0100

    Rendu le yen avec AnimMaker optionnel : si le path d'AnimMaker n'est pas confituré, c'est ok.

--- src/PHPM/Bundle/Entity/AnimLinker.php
+++ src/PHPM/Bundle/Entity/AnimLinker.php
@@ -9,26 +9,31 @@ namespace PHPM\Bundle\Entity;
  
 class AnimLinker
 {
-    protected  $dbpath;
+    protected $dbpath;
     protected $edition;
    
-    public function __construct($dbpath, $edition){
-        $this->dbpath=$dbpath;
-        $this->edition=$edition;
-        
+    public function __construct($dbpath, $edition)
+    {
+        $this->dbpath = $dbpath;
+        $this->edition = $edition;
     }
     
     public function getAnimsArray()
     {
-        
-        $dbhandle = new \SQLite3($this->dbpath);
-        
-        $anims = array();
-        if (!$dbhandle) die ($error);
-        $results = $dbhandle->query("SELECT id,numero,nom FROM anims WHERE edition=$this->edition");
-        while ($row = $results->fetchArray()) {
-            $anims[$row['id']]=($row['nom']);
-        }
+    	$anims = array();
+		
+		// si le clef n'est pas configurée, on ne va pas chercher les anims
+        if ($this->dbpath != '') {
+	        $dbhandle = new \SQLite3($this->dbpath);
+	        
+	        if (!$dbhandle) die ($error);
+			
+	        $results = $dbhandle->query("SELECT id,numero,nom FROM anims WHERE edition=$this->edition");
+			
+	        while ($row = $results->fetchArray()) {
+	            $anims[$row['id']]=($row['nom']);
+	        }
+		}
         
         return $anims;
     }
