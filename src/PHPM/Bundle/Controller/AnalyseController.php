611c5ff706ea7a034b500c870e9e934af0ea231b
Laurent BILLON <laurent.billonc@gmail.com>
2012-04-18 00:02:54 +0200

    add usageMateriel view and enhance top nav bar

--- src/PHPM/Bundle/Controller/AnalyseController.php
+++ src/PHPM/Bundle/Controller/AnalyseController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use PHPM\Bundle\Entity\Config;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 
 /**
@@ -28,49 +29,93 @@ class AnalyseController extends Controller
      */
     public function analyseAction()
     {
-    	if (false === $this->get('security.context')->isGranted('ROLE_ADMIN')) {
+    	if (false === $this->get('security.context')->isGranted('ROLE_USER')) {
+    		throw new AccessDeniedException();
+    	}
+    	
+    
+    	return array(			
+    		);
+    }
+    
+    /**
+     * Statistiques
+     *
+     * @Route("/stats", name="analyse_stats")
+     * @Template()
+     */
+    public function statsAction()
+    {
+    	if (false === $this->get('security.context')->isGranted('ROLE_USER')) {
     		throw new AccessDeniedException();
     	}
     	$em = $this->getDoctrine()->getEntityManager();
-    	
+    	 
     	$tachesResult = $em
     	->createQuery("SELECT e.nom,t.statut,count(t) AS total FROM PHPMBundle:Tache t JOIN t.groupeTache g JOIN g.equipe e GROUP BY g.equipe, t.statut")
     	->getResult();
-    	
+    	 
     	$tachesStats = array();
     	$tachesStatsSum = array("-1"=>0,"0"=>0,"1"=>0,"2"=>0,"3"=>0);
-    	
-    	
+    	 
+    	 
     	foreach ($tachesResult as $i){
     		if(!array_key_exists($i['nom'], $tachesStats)){
     			$tachesStats[$i['nom']]=array("-1"=>0,"0"=>0,"1"=>0,"2"=>0,"3"=>0);
     		}
-        
-    	$tachesStats[$i['nom']][$i['statut']]=$i['total']	;
-    	$tachesStatsSum[$i['statut']]+=$i['total'];
-    		
+    
+    		$tachesStats[$i['nom']][$i['statut']]=$i['total']	;
+    		$tachesStatsSum[$i['statut']]+=$i['total'];
+    
     	}
     	$tachesStats['Total']=$tachesStatsSum;
-    	
-    	
+    	 
+    	 
     	$orgasResult = $em
     	->createQuery("SELECT e.nom as equipe, count(o) as total FROM PHPMBundle:Orga o JOIN o.equipe e GROUP BY e ORDER BY total DESC")
     	->getArrayResult();
-    	 
+    
     	$orgasStats = array();
-    	    	 
+    	 
     	$orgasStatsSum=0;
     	foreach ($orgasResult as $i){
-    	
+    		 
     		$orgasStats[$i['equipe']]=$i['total']	;
     		$orgasStatsSum+=$i['total'];
     	}
     	$orgasStats['Total']=$orgasStatsSum;
     
     	return array(	'tachesStats'=>$tachesStats,
-    					'orgasStats'=>$orgasStats
-    			
-    		);
+    			'orgasStats'=>$orgasStats
+    			 
+    	);
+    }
+    
+    /**
+     * Rapport usage Matériel
+     *
+     * @Route("/usagemateriel", name="analyse_usageMateriel")
+     * @Template()
+     */
+    public function usageMaterielAction()
+    {
+    	if (false === $this->get('security.context')->isGranted('ROLE_USER')) {
+    		throw new AccessDeniedException();
+    	}
+    	    	
+    	$em = $this->getDoctrine()->getEntityManager();
+    	 
+    	$bmDQL = "SELECT m,bm,t,p,g FROM PHPMBundle:Materiel m JOIN m.besoinsMateriel bm JOIN bm.tache t JOIN t.plagesHoraire p LEFT JOIN t.groupeTache g
+    	 WHERE  bm.quantite <> 0 AND t.statut >= 2 ORDER BY p.debut
+    	 ";
+    	    	
+    	
+    	$bmResult = $em
+    	->createQuery($bmDQL)
+    	->getResult();
+    	
+    
+    	return array('bmResult'=>$bmResult);
     }
     
     
