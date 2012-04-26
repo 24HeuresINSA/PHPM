fdc867bc3061693e30a4211c665d171581c0a066
Laurent BILLON <laurent.billonc@gmail.com>
2012-04-27 01:39:18 +0200

    show config code in config view

--- src/PHPM/Bundle/Form/ConfianceType.php
+++ src/PHPM/Bundle/Form/ConfianceType.php
@@ -14,6 +14,7 @@ class ConfianceType extends AbstractType
             ->add('valeur')
             ->add('couleur')
             ->add('privileges')
+            ->add('code')
         ;
     }
     
