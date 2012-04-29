3be424a19ce6505ed36a9f9359f8586abcefe6f4
Romaric Drigon <romaric.drigon@gmail.com>
2012-04-30 01:35:41 +0200

    Viré code déprécié

--- src/PHPM/Bundle/Entity/OrgaRepository.php
+++ src/PHPM/Bundle/Entity/OrgaRepository.php
@@ -21,7 +21,7 @@ class OrgaRepository extends EntityRepository
 		//le distinct est là à cause du creneau_id, en attendant qu'il soit mieux codé ^ ^
 		
 		// (le JOIN plante)ancienne) requête (148b2848650e5c3af0bff2685054605d5ee10944)
-		$dql = "SELECT o, SUM(di.pointsCharisme) charisme FROM PHPMBundle:Orga AS o JOIN o.disponibilitesInscription di JOIN o.disponibilites d JOIN o.equipe e WHERE o.statut=1 AND d.orga != 0";
+		$dql = "SELECT o, SUM(di.pointsCharisme) charisme FROM PHPMBundle:Orga AS o JOIN o.disponibilitesInscription di JOIN o.disponibilites d JOIN o.equipe e WHERE o.statut=1";
 		
  		if ($annee_permis !== '') {
  			if ($annee_permis == 0) {
