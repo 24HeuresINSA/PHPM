fef82503a34613420ded2f840a2a2ac6f1430ecf
Laurent BILLON <laurent.billonc@gmail.com>
2012-04-29 16:45:46 +0200

    reorder dispo by debut in orga index

--- src/PHPM/Bundle/Controller/OrgaController.php
+++ src/PHPM/Bundle/Controller/OrgaController.php
@@ -79,7 +79,7 @@ class OrgaController extends Controller
         ->getResult();
         
         
-        $orgasDQL = "SELECT o,e,d FROM PHPMBundle:Orga o JOIN o.equipe e JOIN e.confiance c LEFT JOIN o.disponibilites d WHERE o.statut = $statut ORDER BY o.lastActivity DESC";
+        $orgasDQL = "SELECT o,e,d FROM PHPMBundle:Orga o JOIN o.equipe e JOIN e.confiance c LEFT JOIN o.disponibilites d WHERE o.statut = $statut ORDER BY o.lastActivity DESC, o.debut";
         
         if ($confiance !='all'){
         	$orgasDQL .= " AND c.id = $confiance";
