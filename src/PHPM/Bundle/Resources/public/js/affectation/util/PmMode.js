6a870d5ac53665793534fda2d09c4819797dd38f
Romaric Drigon <romaric.drigon@gmail.com>
2012-05-13 18:10:15 +0200

    Meilleure encapsulation

--- src/PHPM/Bundle/Resources/public/js/affectation/util/PmMode.js
+++ src/PHPM/Bundle/Resources/public/js/affectation/util/PmMode.js
@@ -34,12 +34,7 @@ PmMode.prototype = {
 		
 		// TODO : off les bind
 		
-		// re-set des valeurs de current
-		pmAffectation.current.orga = {id: -1};
-		pmAffectation.current.jour = -1;
-		pmAffectation.current.quart_heure = -1; // wildcart
-		pmAffectation.current.creneau = {id: -1};
-		pmAffectation.current.tache = {id: -1};
+		pmUtils.setDefault();
 		
 		if (pmAffectation.current.mode === 'orga') {
 			pmAffectation.current.tache.id = -1;
