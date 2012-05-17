b06f30c0543a174f2668e39ea2247011cee33d25
Romaric Drigon <romaric.drigon@gmail.com>
2012-05-17 18:55:29 +0200

    LineFeedFix

--- src/PHPM/Bundle/Entity/CreneauRepository.php
+++ src/PHPM/Bundle/Entity/CreneauRepository.php
@@ -235,7 +235,6 @@ class CreneauRepository extends EntityRepository
 				JOIN PlageHoraire p ON c.plageHoraire_id = p.id
 				JOIN Tache t ON p.tache_id = t.id
 				WHERE t.id = ?';
-		
 				
 		if ($plage_id !== '') {
 			$pref = json_decode($this->getEntityManager()->getRepository('PHPMBundle:Config')->findOneByField('manifestation_plages')->getValue(), TRUE);
