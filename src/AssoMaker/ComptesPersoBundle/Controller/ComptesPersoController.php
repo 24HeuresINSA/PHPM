13272e8f42c108a66f673b7db18586e068c424b5
Laurent BILLON <laurent.billonc@gmail.com>
2012-10-10 11:24:42 +0200

    ajax comptes perso view

--- src/AssoMaker/ComptesPersoBundle/Controller/ComptesPersoController.php
+++ src/AssoMaker/ComptesPersoBundle/Controller/ComptesPersoController.php
@@ -9,8 +9,9 @@ use AssoMaker\ComptesPersoBundle\Form\VirementType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
-
+use Symfony\Component\HttpFoundation\Response;
 
 
 /**
@@ -19,98 +20,131 @@ use Symfony\Component\HttpFoundation\Request;
  * @Route("/comptesPerso")
  */
 class ComptesPersoController extends Controller {
+	    
+    /**
+     * Lists all Orga comptes
+     *
+     * @Route("/", name="comptesPerso")
+     * @Template()
+     */
+    public function comptesPersoAction(Request $request) {
+        if (false === $this->get('security.context')->isGranted('ROLE_ADMIN')) {
+            throw new AccessDeniedException();
+        }
+        $em = $this->getDoctrine()->getEntityManager();
+        $config = $e = $this->get('config.extension');
+        $prixConso = $config->getValue('comptes_perso_prix_conso_standard');
+    
+    
+        $consosBuilder = $this->createFormBuilder();
+        $builder = $this->createFormBuilder();
+        $builder->add('operation', null, array('required' => false));
+        $builder->add($consosBuilder);
+        $form = $builder->getForm();
+    
+    
+        return array('prixConsoStandard' => $prixConso,
+                 'form' => $form->createView());
+    }
+    
+    
+	
 	/**
-	 * Lists all Orga comptes 
+	 * Lists all Orga comptes as JSON
 	 *
-	 * @Route("/", name="comptesPerso")
+	 * @Route("/comptes.json", name="comptesPersoJSON")
+	 * @Method("get")
 	 * @Template()
 	 */
-	public function comptesPersoAction(Request $request) {
+	public function comptesPersoDataAction(Request $request) {
 		if (false === $this->get('security.context')->isGranted('ROLE_ADMIN')) {
 			throw new AccessDeniedException();
 		}
 		$em = $this->getDoctrine()->getEntityManager();
-		$config = $e = $this->get('config.extension');
-		$prixConso = $config->getValue('comptes_perso_prix_conso_standard');
-
+		
 		$entities = $em
-				->createQuery("SELECT o FROM AssoMakerBaseBundle:Orga o")
-				->getResult();
+		->createQuery("SELECT o FROM AssoMakerBaseBundle:Orga o")
+		->getResult();
+		
+		
+		$comptesArray = array();
+		//création du Json de retour selon le modèle définit dans la spec (cf wiki)
+		foreach ($entities as $orga) {
 
-		$consosBuilder = $this->createFormBuilder();
-
-		$totalBalance = 0;
-		$orgas = array();
-		foreach ($entities as $entity) {
-			$orga = array();
-			$orga['id'] = $entity->getId();
-			$orga['nom'] = $entity->getNom();
-			$orga['prenom'] = $entity->getPrenom();
-			$orga['surnom'] = $entity->getSurnom();
-			$orga['balance'] = $em
-					->getRepository('AssoMakerComptesPersoBundle:Transaction')
-					->getOrgaBalance($entity->getId());
-			$orgas[$entity->getId()] = $orga;
-
-			$consosBuilder
-					->add((string) $orga['id'], 'number',
-							array('required' => false));
-
-			$totalBalance += $orga['balance'];
+			$orgaArray[] = array(
+						"id" => $orga->getId(),
+			            "name" => $orga->__toString(),
+			        	"balance"=> $em
+					    ->getRepository('AssoMakerComptesPersoBundle:Transaction')
+					    ->getOrgaBalance($orga->getId())
+						);
 		}
-		;
-
-		$builder = $this->createFormBuilder();
-		$builder->add('operation', null, array('required' => false));
-
-		$builder->add($consosBuilder);
-
-		$form = $builder->getForm();
-
-		if ($request->getMethod() == 'POST') {
-			$form->bind($request);
-
-			if ($form->isValid()) {
-				$data = $form->getData();
-				foreach ($data['form'] as $id => $conso) {
-					if ($conso != null) {
-						$param = $request->request->all();
-						$action = $param['action'];
-
-						if ($action == 'consoStandard') {
-							$nbConsos = $data['form'][$id];
-							$orga = $em
-									->getRepository('AssoMakerBaseBundle:Orga')
-									->find($id);
-							$amount = -$prixConso * $nbConsos;
-							$comment = "$nbConsos consommations à $prixConso € = $amount €";
-						} elseif ($action == 'autre') {
-							$operation = $data['operation'];
-							$amount = $data['form'][$id];
-							$orga = $em
-									->getRepository('AssoMakerBaseBundle:Orga')
-									->find($id);
-							$comment = "$operation : $amount €";
-						} else {
-							$this->getResponse()->setStatusCode('400');
-							return;
-						}
-
-						$transaction = new Transaction($orga, $amount, $comment);
-						$em->persist($transaction);
-					}
-				}
-				$em->flush();
-				return $this->redirect($this->generateUrl('comptesPerso'));
-			}
-		}
-
-		return array('prixConsoStandard' => $prixConso, 'orgas' => $orgas,
-				'totalBalance' => $totalBalance, 'form' => $form->createView());
+		
+    	//exit(var_dump($orgaArray));
+    	
+    	$response = new Response();
+    	$response->setContent(json_encode($orgaArray));
+		$response->headers->set('Content-Type', 'application/json');
+    	
+    	return $response;
 	}
+	
+	/**
+	 * Lists all Orga comptes as JSON
+	 *
+	 * @Route("/process", name="comptesPersoProcess")
+	 * @Method("post")
+	 * @Template()
+	 */
+	public function comptesPersoProcessAction(Request $request) {
+	    if (false === $this->get('security.context')->isGranted('ROLE_ADMIN')) {
+	        throw new AccessDeniedException();
+	    }
+	    $em = $this->getDoctrine()->getEntityManager();
+	    
+	    $data=json_decode($request->getContent(),true);
+	    $config = $e = $this->get('config.extension');
+        $prixConso = $config->getValue('comptes_perso_prix_conso_standard');
+	    
+	    foreach ($data["alter"] as $id => $conso) {
+	        if ($conso != null) {
+	            $param = $request->request->all();
+	    
+	            if ($data['mode'] == 0) {
+	                $nbConsos = $data['alter'][$id];
+	                $orga = $em
+	                ->getRepository('AssoMakerBaseBundle:Orga')
+	                ->find($id);
+	                $amount = -$prixConso * $nbConsos;
+	                $comment = "$nbConsos consommations à $prixConso € = $amount €";
+	            } elseif ($data['mode'] == 1) {
+	                $operation = $data['operation'];
+	                $amount = $data['alter'][$id];
+	                $orga = $em
+	                ->getRepository('AssoMakerBaseBundle:Orga')
+	                ->find($id);
+	                $comment = "$operation : $amount €";
+	            } else {
+	                $this->getResponse()->setStatusCode('400');
+	                return;
+	            }
+	    
+	            
+	            $transaction = new Transaction($orga, $amount, $comment);
+	            $em->persist($transaction);  
+	        }
+	    }
+	    $em->flush();
+	
+	    
+	     
+	    return new Response();
+	}
+	
+	
 
 	/**
-	 * Lists all Orga comptes
+	 * Virement
 	 *
 	 * @Route("/virement", name="comptesPerso_virement")
 	 * @Template()
@@ -147,5 +181,7 @@ class ComptesPersoController extends Controller {
 
 		return array('form' => $form->createView(), 'soldeCP'=>$soldeCP);
 	}
+	
+	
 
 }
