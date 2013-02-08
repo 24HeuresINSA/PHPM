3b3afa184b56314f786fb9e6671752a7e568db1e
Laurent BILLON <laurent.billonc@gmail.com>
2013-02-08 16:33:55 +0100

    enable modules in fixtures

--- src/AssoMaker/BaseBundle/DataFixtures/ORM/BaseConfigFixture.php
+++ src/AssoMaker/BaseBundle/DataFixtures/ORM/BaseConfigFixture.php
@@ -1,113 +1,75 @@
 <?php
-namespace AssoMaker\BaseBundle\DataFixtures\ORM;
-use AssoMaker\PHPMBundle\Entity\Config;
 
+namespace AssoMaker\BaseBundle\DataFixtures\ORM;
+
+use AssoMaker\PHPMBundle\Entity\Config;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
 class BaseConfigFixture implements FixtureInterface {
 
-	public function load(ObjectManager $manager) {
-		echo "Starting to load PHPLanningmaker Config FIXTURES\n";
+    public function load(ObjectManager $manager) {
+        echo "Starting to load PHPLanningmaker Config FIXTURES\n";
 
-		$manager
-		->persist(
-				new Config('phpm_actif',
-						'Module planningmaker activé',
-						'1',
-						NULL));
-		$manager
-		->persist(
-		        new Config('comptes_perso_actif',
-		                'Module Comptes Perso activé',
-		                '0',
-		                NULL));
-		$manager
-		->persist(
-		        new Config('sponso_actif',
-		                'Module Sponso activé',
-		                '0',
-		                NULL));
-		
-		$manager
-		->persist(
-		        new Config('anim_actif',
-		                'Module Anim activé',
-		                '0',
-		                NULL));
-		
-		$manager
-		->persist(
-		        new Config('pass_actif',
-		                'Module Pass activé',
-		                '0',
-		                NULL));
-		$manager
-		->persist(
-		        new Config('base_signature_actif',
-		                'Module signature actif',
-		                '0',
-		                'Autoriser les orgas à générer leur signature'));
+        $manager
+                ->persist(
+                        new Config('phpm_actif', 'Module planningmaker activé', '1', NULL));
+        $manager
+                ->persist(
+                        new Config('comptes_perso_actif', 'Module Comptes Perso activé', '1', NULL));
+        $manager
+                ->persist(
+                        new Config('sponso_actif', 'Module Sponso activé', '1', NULL));
 
- 		$manager
-				->persist(
-						new Config('base_admin_login',
-								'Login admin automatique', '1',
-								NULL)); 
-		$manager
-				->persist(
-						new Config('manifestation_nom',
-								'Nom de la manifestation',
-								'24 Heures de l\'INSA 38e', NULL));
-		$manager
-				->persist(
-						new Config('manifestation_edition',
-								'Numéro de l\'édition', '38', NULL));
-		$manager
-				->persist(
-						new Config('phpm_contenu_page_principale',
-								'Contenu de la page principale',
-								'<h1>PHPlanningMaker<h2><p>Gestion des ressources humaines.</p>',
-								NULL));
-		
-		$manager
-				->persist(
-						new Config('phpm_admin_email',
-								'Email de l\'administrateur',
-								'orga@24heures.org', NULL));
-		
-		$manager
-				->persist(
-						new Config('phpm_secret_salt',
-								'Clé secrète pour générer les URL de login',
-								'xxxx', NULL));
-		$manager
-				->persist(
-						new Config('phpm_tache_heure_limite_validation',
-								'Heure limite pour envoyer les fiches tâches en validation',
-								'2012-05-06 20:00:00', NULL));
-		$manager
-				->persist(
-						new Config('phpm_planning_visiteurs',
-								'Autoriser les visiteurs à accéder à leur planning',
-								0, NULL));
-		$manager
-				->persist(
-						new Config('phpm_contenu_mail_planning',
-								'Contenu du mail de notification de planning',
-								'<p>Ton planning est disponible.</p>\r\n<p><em>Maud et Adam.</em></p>',
-								NULL));	
-		$manager
-    		->persist(
-    		        new Config('phpm_ws_access_code',
-    		                'Code des WebServices',
-    		                mt_rand(),
-    		                NULL));
-	
-		
-		$manager->flush();
+        $manager
+                ->persist(
+                        new Config('anim_actif', 'Module Anim activé', '1', NULL));
 
-		//         $manager->persist($userAdmin);
-		//         $manager->flush();
-	}
+        $manager
+                ->persist(
+                        new Config('pass_actif', 'Module Pass activé', '1', NULL));
+        $manager
+                ->persist(
+                        new Config('base_signature_actif', 'Module signature actif', '1', 'Autoriser les orgas à générer leur signature'));
+
+        $manager
+                ->persist(
+                        new Config('base_admin_login', 'Login admin automatique', '1', NULL));
+        $manager
+                ->persist(
+                        new Config('manifestation_nom', 'Nom de la manifestation', '24 Heures de l\'INSA 38e', NULL));
+        $manager
+                ->persist(
+                        new Config('manifestation_edition', 'Numéro de l\'édition', '38', NULL));
+        $manager
+                ->persist(
+                        new Config('phpm_contenu_page_principale', 'Contenu de la page principale', '<h1>PHPlanningMaker<h2><p>Gestion des ressources humaines.</p>', NULL));
+
+        $manager
+                ->persist(
+                        new Config('phpm_admin_email', 'Email de l\'administrateur', 'orga@24heures.org', NULL));
+
+        $manager
+                ->persist(
+                        new Config('phpm_secret_salt', 'Clé secrète pour générer les URL de login', 'xxxx', NULL));
+        $manager
+                ->persist(
+                        new Config('phpm_tache_heure_limite_validation', 'Heure limite pour envoyer les fiches tâches en validation', '2012-05-06 20:00:00', NULL));
+        $manager
+                ->persist(
+                        new Config('phpm_planning_visiteurs', 'Autoriser les visiteurs à accéder à leur planning', 0, NULL));
+        $manager
+                ->persist(
+                        new Config('phpm_contenu_mail_planning', 'Contenu du mail de notification de planning', '<p>Ton planning est disponible.</p>\r\n<p><em>Maud et Adam.</em></p>', NULL));
+        $manager
+                ->persist(
+                        new Config('phpm_ws_access_code', 'Code des WebServices', mt_rand(), NULL));
+
+
+        $manager->flush();
+
+        //         $manager->persist($userAdmin);
+        //         $manager->flush();
+    }
+
 }
--- src/AssoMaker/BaseBundle/DataFixtures/ORM/BaseEntitiesFixture.php
+++ src/AssoMaker/BaseBundle/DataFixtures/ORM/BaseEntitiesFixture.php
@@ -1,74 +1,73 @@
 <?php
+
 namespace AssoMaker\BaseBundle\DataFixtures\ORM;
+
 use AssoMaker\BaseBundle\Entity\Orga;
-
 use AssoMaker\BaseBundle\Entity\Equipe;
-
 use AssoMaker\BaseBundle\Entity\Confiance;
-
 use AssoMaker\PHPMBundle\Entity\Config;
-
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
 class BaseEntitiesFixture implements FixtureInterface {
 
-	public function load(ObjectManager $manager) {
-		echo "Starting to load PHPLanningmaker Entities FIXTURES\n";
+    public function load(ObjectManager $manager) {
+        echo "Starting to load PHPLanningmaker Entities FIXTURES\n";
 
-		//Confiances
-		
-		$softConfiance = new Confiance();
-		$softConfiance->setNom('Soft');
-		$softConfiance->setValeur(100);
-		$softConfiance->setCouleur('blue');
-		$softConfiance->setCode(mt_rand());
-		$softConfiance->setPrivileges(0);
-		$manager->persist($softConfiance);
-		
-		$confianceConfiance = new Confiance();
-		$confianceConfiance->setNom('Confiance');
-		$confianceConfiance->setValeur(500);
-		$confianceConfiance->setCouleur('green');
-		$confianceConfiance->setCode(mt_rand());
-		$confianceConfiance->setPrivileges(0);
-		$manager->persist($confianceConfiance);
-		
-		$hardConfiance = new Confiance();
-		$hardConfiance->setNom('Hard');
-		$hardConfiance->setValeur(1000);
-		$hardConfiance->setCouleur('orange');
-		$hardConfiance->setCode(mt_rand());
-		$hardConfiance->setPrivileges(1);
-		$manager->persist($hardConfiance);
-		
-		$hardEquipe = new Equipe();
-		$hardEquipe->setConfiance($hardConfiance);
-		$hardEquipe->setCouleur($hardConfiance->getCouleur());
-		$hardEquipe->setNom($hardConfiance->getNom());
-		
-		$admin = new Orga();
-		$admin->setNom("Admin");
-		$admin->setPrenom("Admin");
-		$admin->setAnneeEtudes(0);
-		$admin->setDateDeNaissance(new \DateTime());
-		$admin->setDepartement("Admin");
-		$admin->setTelephone("0699999999");
-		$admin->setEmail('admin@admin.com');
-		$admin->setEquipe($hardEquipe);
-		$admin->setStatut($hardEquipe);
-		$admin->setPrivileges(2);
-		
-		
-		$hardEquipe->setResponsable($admin);
-		
-		$manager->persist($hardEquipe);
-		$manager->persist($admin);
+        //Confiances
 
-		
-		
-		
-		
-		$manager->flush();
-	}
+        $softConfiance = new Confiance();
+        $softConfiance->setNom('Soft');
+        $softConfiance->setValeur(100);
+        $softConfiance->setCouleur('blue');
+        $softConfiance->setCode(mt_rand());
+        $softConfiance->setPrivileges(0);
+        $manager->persist($softConfiance);
+
+        $confianceConfiance = new Confiance();
+        $confianceConfiance->setNom('Confiance');
+        $confianceConfiance->setValeur(500);
+        $confianceConfiance->setCouleur('green');
+        $confianceConfiance->setCode(mt_rand());
+        $confianceConfiance->setPrivileges(0);
+        $manager->persist($confianceConfiance);
+
+        $hardConfiance = new Confiance();
+        $hardConfiance->setNom('Hard');
+        $hardConfiance->setValeur(1000);
+        $hardConfiance->setCouleur('orange');
+        $hardConfiance->setCode(mt_rand());
+        $hardConfiance->setPrivileges(1);
+        $manager->persist($hardConfiance);
+
+        $hardEquipe = new Equipe();
+        $hardEquipe->setConfiance($hardConfiance);
+        $hardEquipe->setCouleur($hardConfiance->getCouleur());
+        $hardEquipe->setNom($hardConfiance->getNom());
+
+        $admin = new Orga();
+        $admin->setNom("Admin");
+        $admin->setPrenom("Admin");
+        $admin->setAnneeEtudes(0);
+        $admin->setDateDeNaissance(new \DateTime());
+        $admin->setDepartement("Admin");
+        $admin->setTelephone("0699999999");
+        $admin->setEmail('admin@admin.com');
+        $admin->setEquipe($hardEquipe);
+        $admin->setStatut($hardEquipe);
+        $admin->setPrivileges(2);
+
+
+        $hardEquipe->setResponsable($admin);
+
+        $manager->persist($hardEquipe);
+        $manager->persist($admin);
+
+
+
+
+
+        $manager->flush();
+    }
+
 }
