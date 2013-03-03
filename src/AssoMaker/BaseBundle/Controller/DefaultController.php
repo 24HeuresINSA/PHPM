d021921a4b7d5d5e344570db097585b02f95a1fd
Laurent BILLON <laurent.billonc@gmail.com>
2013-03-03 19:00:45 +0100

    fix redirect upon successful login

--- src/AssoMaker/BaseBundle/Controller/DefaultController.php
+++ src/AssoMaker/BaseBundle/Controller/DefaultController.php
@@ -13,94 +13,92 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use AssoMaker\PHPMBundle\Entity\Config;
 use AssoMaker\PHPMBundle\Form\PrintPlanningType;
+use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
-{
-	
-/**
-     * @Route("/", name="base_accueil")
-	 * @Route("/")
+class DefaultController extends Controller {
+
+    /**
+     * @Route("/home", name="base_accueil")
+     * @Route("/home")
      * @Template()
      */
-    public function homeAction()
-    {
-    	$em = $this->getDoctrine()->getEntityManager();
-    	$config = $this->get('config.extension');
-    	   	
-    	$user=$this->get('security.context')->getToken()->getUser();
-    	
-    	if (!$this->get('security.context')->isGranted('ROLE_VISITOR')) {
-    		if ($config->getValue('base_admin_login')==1){
-    			return $this->adminLogin();
-    		}
-    		return array();
-    	}
-    	
-    	$statsUser=$em->getRepository('AssoMakerBaseBundle:Orga')->getStats($user);
-    	$soldeCP =  $em->getRepository('AssoMakerComptesPersoBundle:Transaction')->getOrgaBalance($user->getId());
-    	$transactionsCP = $em->getRepository('AssoMakerComptesPersoBundle:Transaction')->getTransactionsByOrga($user->getId());
-    	
-    	$statsUser['taches']=$em->getRepository('AssoMakerPHPMBundle:Tache')->getOrgaStats($user);
-    	$debutPlanning = new \DateTime();
-    	$finPlanning = new \DateTime($config->getValue('phpm_planning_fin'));
-    	$planning=$em->getRepository('AssoMakerBaseBundle:Orga')->getPlanning($user->getId(),null,$debutPlanning,$finPlanning);
-    	$deadlineFT=$config->getValue('phpm_tache_heure_limite_validation');
-    	 
-    	$printPlanningForm = $this->createForm(new PrintPlanningType(), array('debut'=>new \DateTime(),'fin'=>new \DateTime()));
-    	
-    	
-    	return array('soldeCP'=>$soldeCP,
-    	            'transactionsCP'=>$transactionsCP,
-    	            'statsOrga'=>$statsUser,
-    				'planning'=>$planning,
-    				'debutPlanning'=>$debutPlanning,
-    				'finPlanning'=>$finPlanning,
-    				'deadlineFT'=>$deadlineFT,
-    				'printPlanningForm'=>$printPlanningForm->createView()
-    				);
-    	
-        
+    public function homeAction() {
+        $em = $this->getDoctrine()->getEntityManager();
+        $config = $this->get('config.extension');
+
+        $user = $this->get('security.context')->getToken()->getUser();
+
+        if (!$this->get('security.context')->isGranted('ROLE_VISITOR')) {
+            if ($config->getValue('base_admin_login') == 1) {
+                return $this->adminLogin();
+            }
+            return array();
+        }
+
+        $statsUser = $em->getRepository('AssoMakerBaseBundle:Orga')->getStats($user);
+        $soldeCP = $em->getRepository('AssoMakerComptesPersoBundle:Transaction')->getOrgaBalance($user->getId());
+        $transactionsCP = $em->getRepository('AssoMakerComptesPersoBundle:Transaction')->getTransactionsByOrga($user->getId());
+
+        $statsUser['taches'] = $em->getRepository('AssoMakerPHPMBundle:Tache')->getOrgaStats($user);
+        $debutPlanning = new \DateTime();
+        $finPlanning = new \DateTime($config->getValue('phpm_planning_fin'));
+        $planning = $em->getRepository('AssoMakerBaseBundle:Orga')->getPlanning($user->getId(), null, $debutPlanning, $finPlanning);
+        $deadlineFT = $config->getValue('phpm_tache_heure_limite_validation');
+
+        $printPlanningForm = $this->createForm(new PrintPlanningType(), array('debut' => new \DateTime(), 'fin' => new \DateTime()));
+
+
+        return array('soldeCP' => $soldeCP,
+            'transactionsCP' => $transactionsCP,
+            'statsOrga' => $statsUser,
+            'planning' => $planning,
+            'debutPlanning' => $debutPlanning,
+            'finPlanning' => $finPlanning,
+            'deadlineFT' => $deadlineFT,
+            'printPlanningForm' => $printPlanningForm->createView()
+        );
     }
-    
+
+    /**
+     * @Route("/", name="base_publichome")
+     * @Route("/")
+     * @Template()
+     */
+    public function publicHomeAction() {
+
+        return array();
+    }
+
     /**
      * Link login
      *
      * @Route("/autologin/{id}/{loginkey}",requirements={"loginkey" = ".+"}, name="autologin")
      *
      */
-    public function autologinAction($id,$loginkey)
-    {
-    	$em = $this->getDoctrine()->getEntityManager();
-    	$config = $this->get('config.extension');
-    	 
-    	$user = $em->getRepository('AssoMakerBaseBundle:Orga')->findOneById($id);
-    	$secretSalt=$config->getValue('phpm_secret_salt');
-    	if ((!$user)||($loginkey!==crypt($secretSalt.$id, 24))) {
-    		return $this->redirect($config->getValue('phpm_orgasoft_inscription_returnURL'));
-    	}
-    	 
-    	$this->get('security.context')->setToken($user->generateUserToken());
-    	 
-    	return $this->redirect($this->generateUrl('base_accueil'));
-    	 
-    	 
-    	 
-    	 
+    public function autologinAction($id, $loginkey) {
+        $em = $this->getDoctrine()->getEntityManager();
+        $config = $this->get('config.extension');
+
+        $user = $em->getRepository('AssoMakerBaseBundle:Orga')->findOneById($id);
+        $secretSalt = $config->getValue('phpm_secret_salt');
+        if ((!$user) || ($loginkey !== crypt($secretSalt . $id, 24))) {
+            return $this->redirect($config->getValue('phpm_orgasoft_inscription_returnURL'));
+        }
+
+        $this->get('security.context')->setToken($user->generateUserToken());
+
+        return $this->redirect($this->generateUrl('base_accueil'));
     }
-    
-    
-    public function adminLogin()
-    {
-    	$em = $this->getDoctrine()->getEntityManager();
-    	$config = $this->get('config.extension');
-    	$orgas = $em->getRepository('AssoMakerBaseBundle:Orga')->findAll();
-    	$admin = $orgas[0];
-    	$this->get('security.context')->setToken($admin->generateUserToken());
-    	return $this->redirect($this->generateUrl('base_accueil'));
-    
+
+    public function adminLogin() {
+        $em = $this->getDoctrine()->getEntityManager();
+        $config = $this->get('config.extension');
+        $orgas = $em->getRepository('AssoMakerBaseBundle:Orga')->findAll();
+        $admin = $orgas[0];
+        $this->get('security.context')->setToken($admin->generateUserToken());
+        return $this->redirect($this->generateUrl('base_accueil'));
     }
-    
-    
+
     /**
      * OpenId login
      *
@@ -108,76 +106,76 @@ class DefaultController extends Controller
      * @Template()
      *
      */
-    public function loginAction($confianceCode)
-    {
-    
-    	$em = $this->getDoctrine()->getEntityManager();
-    	$config = $this->get('config.extension');
-    	$session = $this->getRequest()->getSession();
-    	$serverurl = $this->getRequest()->getHost();
-    	 
-    	try {
-    
-    
-    		$openid = new \LightOpenID($serverurl);
-    		if(!$openid->mode) {
-    			//                 if(isset($_GET['login'])) {
-    			$openid->identity = 'https://www.google.com/accounts/o8/id';
-    			$openid->required = array('contact/email');
-    			
-    			$confiance = $em->getRepository('AssoMakerBaseBundle:Confiance')->findOneByCode($confianceCode);
-    			if($confiance){
-    			   $session->set('confianceCode', $confianceCode);
+    public function loginAction(Request $request, $confianceCode) {
 
-    			}
-    			
-    			//header('Location: ' . $openid->authUrl());
-    
-    			$response = new RedirectResponse($openid->authUrl());
-    			//$response->headers->set('Location:' , $openid->authUrl());
-    
-    			return $response;
-    
-    			//                 }
-    			
-    
-    		} elseif($openid->mode == 'cancel') {
-    			exit;
-    		} else {
-    			$attrs = $openid->getAttributes();
-    			$email = $attrs['contact/email'];
-    			$session->set('email', $email);
-    			$em = $this->getDoctrine()->getEntityManager();
-    			$user = $em->getRepository('AssoMakerBaseBundle:Orga')->findOneByEmail($email);
-/*    
-    			$confiance = $em->getRepository('AssoMakerBaseBundle:Confiance')->findOneByCode($confianceCode);
-    			if(!$confiance){
-    			    throw new AccessDeniedException();
-    			}
-    			
-    			*/
-    			if (!$user) {
-    			    if($this->getRequest()->getSession()->get('confianceCode')){
-    			        return $this->redirect($this->generateUrl('orga_register_user'));
-    			    }
-    				
-    			    $this->get('session')->setFlash('notice', "L'adresse $email n'est pas dans le système.");
-    			    $redirectURL = $config->getValue('manifestation_permis_libelles');
-    				return $this->redirect($this->generateUrl('base_accueil'));
-    			}
-    
-    			$this->get('security.context')->setToken($user->generateUserToken());
-    
-    
-    
-    			return $this->redirect($this->generateUrl('base_accueil'));
-    		}
-    	} catch(ErrorException $e) {
-    		exit;
-    	}
-    
-    	 
-    	 
+
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $config = $this->get('config.extension');
+        $session = $this->getRequest()->getSession();
+        $serverurl = $this->getRequest()->getHost();
+
+
+
+
+        try {
+
+
+            $openid = new \LightOpenID($serverurl);
+            if (!$openid->mode) {
+
+                $session->set('originalUrl', $this->container->get('request')->server->get('PHP_SELF'));
+                //                 if(isset($_GET['login'])) {
+                $openid->identity = 'https://www.google.com/accounts/o8/id';
+                $openid->required = array('contact/email');
+
+                $confiance = $em->getRepository('AssoMakerBaseBundle:Confiance')->findOneByCode($confianceCode);
+                if ($confiance) {
+                    $session->set('confianceCode', $confianceCode);
+                }
+
+                //header('Location: ' . $openid->authUrl());
+
+                $response = new RedirectResponse($openid->authUrl());
+                //$response->headers->set('Location:' , $openid->authUrl());
+
+                return $response;
+
+                //                 }
+            } elseif ($openid->mode == 'cancel') {
+                exit;
+            } else {
+                $attrs = $openid->getAttributes();
+                $email = $attrs['contact/email'];
+                $session->set('email', $email);
+                $em = $this->getDoctrine()->getEntityManager();
+                $user = $em->getRepository('AssoMakerBaseBundle:Orga')->findOneByEmail($email);
+                /*
+                  $confiance = $em->getRepository('AssoMakerBaseBundle:Confiance')->findOneByCode($confianceCode);
+                  if(!$confiance){
+                  throw new AccessDeniedException();
+                  }
+
+                 */
+                if (!$user) {
+                    if ($this->getRequest()->getSession()->get('confianceCode')) {
+                        return $this->redirect($this->generateUrl('orga_register_user'));
+                    }
+
+                    $this->get('session')->setFlash('notice', "L'adresse $email n'est pas dans le système.");
+                    $redirectURL = $config->getValue('manifestation_permis_libelles');
+                    return $this->redirect($this->generateUrl('base_accueil'));
+                }
+
+                $this->get('security.context')->setToken($user->generateUserToken());
+
+
+
+                return $this->redirect($session->get('originalUrl'));
+            }
+        } catch (ErrorException $e) {
+            exit;
+        }
     }
-    
+
 }
