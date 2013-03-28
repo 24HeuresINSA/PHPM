5b74f06e9edd7df1f6587e9051465524e1ea9d60
Laurent BILLON <laurent.billonc@gmail.com>
2013-03-28 11:26:23 +0100

    add register links

--- src/AssoMaker/PHPMBundle/Form/EquipeType.php
+++ src/AssoMaker/PHPMBundle/Form/EquipeType.php
@@ -5,29 +5,27 @@ namespace AssoMaker\PHPMBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 
-class EquipeType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+class EquipeType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-            ->add('nom')
-            ->add('couleur')
-            ->add('responsable',null,array('required'=>false,'label'=>false))
-            ->add('confiance')
-            ->add('showOnTrombi',null,array('required'=>false,'label'=>false))
-            ->add('comptesPersoEnabled',null,array('required'=>false,'label'=>false))
+                ->add('nom')
+                ->add('couleur')
+                ->add('responsable', null, array('required' => false, 'label' => false))
+                ->add('confiance', 'entity', array('class' => 'AssoMaker\BaseBundle\Entity\Confiance'))
+                ->add('showOnTrombi', null, array('required' => false, 'label' => false))
+                ->add('comptesPersoEnabled', null, array('required' => false, 'label' => false))
         ;
     }
-    
-    public function getDefaultOptions(array $options)
-    {
+
+    public function getDefaultOptions(array $options) {
         return array(
-                'data_class' => 'AssoMaker\BaseBundle\Entity\Equipe',
+            'data_class' => 'AssoMaker\BaseBundle\Entity\Equipe',
         );
     }
 
-    public function getName()
-    {
+    public function getName() {
         return 'phpm_bundle_equipetype';
     }
+
 }
