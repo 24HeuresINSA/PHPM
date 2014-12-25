95da66848a3e13b4368f9ee8b6044c598fb58bfb
Philippe Vienne <philippe.vienne@insa-lyon.fr>
2014-12-25 15:46:19 +0100

    New registration

--- src/AssoMaker/BaseBundle/Controller/OAuthController.php
+++ src/AssoMaker/BaseBundle/Controller/OAuthController.php
@@ -56,7 +56,6 @@ class OAuthController extends Controller {
 
     /**
      * @Route("/register", name="register_oauth")
-     * @Template("AssoMakerBaseBundle:Orga:registration.html.twig")
      * Cette action vérifie que l'utilisateur a remplis son inscription et que le compte est actif.
      */
     public function registerAction(Request $request){
@@ -83,118 +82,122 @@ class OAuthController extends Controller {
             return $this->refuseRegistration($em,$entity);
         }
 
-        $form = $this->createForm($this->get('form.type.registration'), $entity, array());
-
-        $orga = $entity;
         $config = $e = $this->get('config.extension');
-        $confianceOrga = $registrationToken->getEquipe()->getConfiance()->getValeur();
-        $groupesDIresult = $this->getDoctrine()->getEntityManager()->createQuery("SELECT g FROM AssoMakerPHPMBundle:Mission g WHERE g.confianceMin <= $confianceOrga ORDER BY g.ordre")->getResult();
-        $groupesDI = array();
 
-        foreach ($groupesDIresult as $entity) {
-            $groupesDI[$entity->getId()] = $entity;
-        }
+        if($request->getSession()->get('registration_step')==null||$request->getSession()->get('registration_step')==0){
+            $request->getSession()->set('registration_step',0);
 
-        $queryResult = $this->getDoctrine()->getEntityManager()->createQuery("SELECT d FROM AssoMakerPHPMBundle:DisponibiliteInscription d JOIN d.mission m WHERE m.confianceMin <= $confianceOrga ORDER BY d.debut")->getResult();
-        $DIs = array();
+            // User is on first step for registration and should enter personal information
 
-        foreach ($queryResult as $entity) {
-            $fmt = new \IntlDateFormatter(null, \IntlDateFormatter::FULL, \IntlDateFormatter::FULL, 'Europe/Paris', null, 'EEEE d MMMM');
-            $DIs[$entity->getMission()->getId()][$fmt->format($entity->getDebut()->getTimestamp())][$entity->getId()] = $entity;
-        }
+            $form = $this->createForm($this->get('form.type.registration'), $entity, array());
 
-        $formDispos = $this->createForm(new InputDisposType());
+            if ($request->getMethod() == 'POST') {
+                $form->handleRequest($request);
 
-        if ($request->getMethod() == 'POST') {
-            $formDispos->handleRequest($request);
-            $form->handleRequest($request);
-            $data = $formDispos->getData();
-            $submittedDI = $data['disponibiliteInscriptionItems'];
+                if ($form->isValid()) {
+                    $em->persist($entity);
+                    $em->flush();
+                    $request->getSession()->set('registration_step', 1);
+                    return $this->redirect($this->generateUrl("register_oauth"));
+                }
+            }
+        } else {
+            $request->getSession()->set('registration_step',1);
 
-            if($form->isValid()){
-                exit;
+            // User is on second step and should enter his availabilities
+
+            $orga = $entity;
+            $confianceOrga = $registrationToken->getEquipe()->getConfiance()->getValeur();
+            $groupesDIresult = $this->getDoctrine()->getEntityManager()->createQuery("SELECT g FROM AssoMakerPHPMBundle:Mission g WHERE g.confianceMin <= $confianceOrga ORDER BY g.ordre")->getResult();
+            $groupesDI = array();
+
+            foreach ($groupesDIresult as $e) {
+                $groupesDI[$e->getId()] = $e;
             }
 
-            if ($form->isValid()&&$formDispos->isValid()) {
-                $entity->setEnabled(true);
-                $entity->addRole('ROLE_ORGA');
-                $entity->setStatut(0);
-                $entity->setEquipe($registrationToken->getEquipe());
-                if($em->getRepository('AssoMakerBaseBundle:Orga')->count()<=1)
-                    $entity->addRole('ROLE_SUPER_ADMIN');
-                if($registrationToken->getCount()<=2) {
-                    $em->remove($registrationToken);
-                }else{
-                    $registrationToken->oneUse();
-                    $em->persist($registrationToken);
-                }
-                $em->persist($entity);
-                $em->flush();
-                $this->container->get('security.context')->setToken(NULL);
+            $queryResult = $this->getDoctrine()->getEntityManager()->createQuery("SELECT d FROM AssoMakerPHPMBundle:DisponibiliteInscription d JOIN d.mission m WHERE m.confianceMin <= $confianceOrga ORDER BY d.debut")->getResult();
+            $DIs = array();
 
-                $allDI = $this->getDoctrine()->getEntityManager()->createQuery("SELECT d FROM AssoMakerPHPMBundle:DisponibiliteInscription d")->getResult();
-
-                $minCharisme = $config->getValue('manifestation_charisme_minimum');
-
-                $totalCharisme = 0;
-
-                foreach ($submittedDI as $di) {
-                    $totalCharisme+=$di->getPointsCharisme();
-                }
-                $totalCharisme+=$orga->getCharisme();
-
-                if ($totalCharisme < $minCharisme) {
-                    return array('form' => $form->createView(),
-                        'entities' => $DIs,
-                        'missions' => $groupesDI,
-                        'orga' => $orga,
-                        'charismeInsuffisant' => true,
-                        'now' => new \DateTime(),
-                        'messagesCharisme' => $config->getValue('phpm_messages_charisme')
-                    );
-                }
-
-                foreach ($allDI as $di) {
-                    if (!$submittedDI->contains($di)) {
-                        if ($orga->getDisponibilitesInscription()->contains($di) && ($this->get('security.context')->isGranted('ROLE_HUMAIN')) && ($di->getDebut() > new \DateTime())) {
-                            $orga->removeDisponibiliteInscription($di);
-                        }
-                    }
-                }
-
-                foreach ($allDI as $di) {
-
-                    if ($submittedDI->contains($di)) {
-                        if (!$orga->getDisponibilitesInscription()->contains($di) && ($this->get('security.context')->isGranted('ROLE_HUMAIN') || ($di->getStatut() > 0)) && ($di->getDebut() > new \DateTime())) {
-                            $orga->addDisponibiliteInscription($di);
-                            $di->addOrga($orga);
-                        }
-                    }
-                }
-
-                $orga->cleanDisponibilites();
-
-                if ($orga->getStatut() == 2) {
-                    $orga->setStatut(1);
-                }
-
-                $em->flush();
-
-                return $this->redirect($this->generateUrl('orga_thankyou'));
+            foreach ($queryResult as $e) {
+                $fmt = new \IntlDateFormatter(null, \IntlDateFormatter::FULL, \IntlDateFormatter::FULL, 'Europe/Paris', null, 'EEEE d MMMM');
+                $DIs[$e->getMission()->getId()][$fmt->format($e->getDebut()->getTimestamp())][$e->getId()] = $e;
             }
+
+            $form = $this->createForm(new InputDisposType());
+
+            if ($request->getMethod() == 'POST') {
+                $form->handleRequest($request);
+                $data = $form->getData();
+                $submittedDI = $data['disponibiliteInscriptionItems'];
+
+                if ($form->isValid()) {
+                    $entity->addRole('ROLE_ORGA');
+                    $entity->setStatut(0);
+                    $entity->setEnabled(true);
+                    $entity->setEquipe($registrationToken->getEquipe());
+                    if($em->getRepository('AssoMakerBaseBundle:Orga')->count()<=1)
+                        $entity->addRole('ROLE_SUPER_ADMIN');
+                    if($registrationToken->getCount()<=2) {
+                        $em->remove($registrationToken);
+                    }else{
+                        $registrationToken->oneUse();
+                        $em->persist($registrationToken);
+                    }
+                    $em->persist($entity);
+                    $em->flush();
+
+                    // Flush authentication to reload permissions
+                    $this->container->get('security.context')->getToken()->setAuthenticated( false );
+
+                    $allDI = $this->getDoctrine()->getEntityManager()->createQuery("SELECT d FROM AssoMakerPHPMBundle:DisponibiliteInscription d")->getResult();
+
+                    foreach ($allDI as $di) {
+                        if (!$submittedDI->contains($di)) {
+                            if ($orga->getDisponibilitesInscription()->contains($di) && ($this->get('security.context')->isGranted('ROLE_HUMAIN')) && ($di->getDebut() > new \DateTime())) {
+                                $orga->removeDisponibiliteInscription($di);
+                            }
+                        }
+                    }
+
+                    foreach ($allDI as $di) {
+
+                        if ($submittedDI->contains($di)) {
+                            if (!$orga->getDisponibilitesInscription()->contains($di) && ($this->get('security.context')->isGranted('ROLE_HUMAIN') || ($di->getStatut() > 0)) && ($di->getDebut() > new \DateTime())) {
+                                $orga->addDisponibiliteInscription($di);
+                                $di->addOrga($orga);
+                            }
+                        }
+                    }
+
+                    $orga->cleanDisponibilites();
+
+                    if ($orga->getStatut() == 2) {
+                        $orga->setStatut(1);
+                    }
+
+                    $em->persist($orga);
+                    $em->flush();
+
+                    $request->getSession()->getFlashBag()->add('info', "Votre inscription est validée mais vous devez vous reconnecter");
+
+                    return $this->redirect($this->generateUrl('orga_thankyou'));
+                }
+            }
+
         }
 
+        $template = $request->getSession()->get('registration_step')<=0?
+            "AssoMakerBaseBundle:Orga:registration.html.twig":
+            "AssoMakerBaseBundle:Orga:registrationInputDispos.html.twig";
 
-        return array(
-            'entity' => $orga,
+        return $this->render($template, array(
             'form' => $form->createView(),
-            'formDispo' => $formDispos->createView(),
-            'entities' => $DIs,
-            'missions' => $groupesDI,
-            'orga' => $orga,
+            'entities' => isset($DIs)?$DIs:null,
+            'missions' => isset($groupesDI)?$groupesDI:null,
+            'orga' => isset($orga)?$orga:null,
             'now' => new \DateTime(),
             'messagesCharisme' => $config->getValue('phpm_messages_charisme')
-        );
+        ));
     }
 
     public function generateAction($equipe_id){
