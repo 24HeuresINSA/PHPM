359ce67822b90c65e7a8923ac2adbf0524e2ba12
Philippe Vienne <philippe.vienne@insa-lyon.fr>
2015-02-22 13:51:53 +0100

    Generate full Planning with PDF

--- src/AssoMaker/BaseBundle/Controller/OrgaController.php
+++ src/AssoMaker/BaseBundle/Controller/OrgaController.php
@@ -2,6 +2,7 @@
 
 namespace AssoMaker\BaseBundle\Controller;
 
+use Doctrine\ORM\Query\ResultSetMapping;
 use Symfony\Component\Security\Core\Authentication\Token\AnonymousToken;
 use JMS\SecurityExtraBundle\Annotation\Secure;
 use AssoMaker\PHPMBundle\Form\InputDisposType;
@@ -430,6 +431,59 @@ class OrgaController extends Controller {
             );
         } catch(\Exception $e){
             return new Response($pdfGenerator->generatePDF("Erreur : ".$e->getMessage()),
+                200,
+                array(
+                    'Content-Type' => 'application/pdf',
+                    'Content-Disposition' => 'inline; filename="out.pdf"'
+                )
+            );
+        }
+
+    }
+
+    /**
+     * Print all orga summary planning.
+     *
+     * @Route("/printAll",  name="orga_print_all")
+     */
+    public function printAllAction() {
+        $config = $this->get('config.extension');
+
+        if (false === $this->get('security.context')->isGranted('ROLE_HUMAIN')) {
+            throw new AccessDeniedException();
+        }
+
+        $debut = new \DateTime();
+        $fin = new \DateTime($config->getValue('phpm_planning_fin'));
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $pdfGenerator = $this->get('spraed.pdf.generator');
+        {
+
+            $orgas = $em->createQuery("SELECT o,d,cr,p,t,g,r FROM AssoMakerBaseBundle:Orga o JOIN o.disponibilites d JOIN d.creneaux cr JOIN
+					cr.plageHoraire p JOIN p.tache t JOIN t.groupeTache g JOIN t.responsable r
+					ORDER BY o.nom,d.debut,cr.debut")->execute();
+
+            $taches = $em->createQueryBuilder()->
+            select("t,g,r")->from("AssoMakerPHPMBundle:Tache","t")->
+            join("t.groupeTache",'g')->join("t.responsable",'r')
+                ->where("t.statut>=2")->orderBy("t.id")->getQuery()->getArrayResult();
+            $max = count($taches);
+            $request = "SELECT o0_.id AS oid, o0_.nom AS nom, o0_.prenom AS prenom, o0_.telephone AS telephone, c1_.debut AS debut, c1_.fin AS fin
+FROM Creneau c1_
+INNER JOIN Disponibilite d2_ ON ( d2_.id = c1_.disponibilite_id )
+INNER JOIN PlageHoraire p3_ ON ( c1_.plageHoraire_id = p3_.id )
+INNER JOIN Tache t4_ ON ( t4_.id = p3_.tache_id )
+INNER JOIN Orga o0_ ON ( o0_.id = d2_.orga_id )
+WHERE t4_.id = ?";
+            for($i = 0; $i < $max; $i++){
+                $result = $em->getConnection()->fetchAll($request,array('0'=>$taches[$i]['id']));
+                $taches[$i]["creneaux"]=$result;
+            }
+
+            $html = $this->renderView('AssoMakerBaseBundle:Orga:printAllPlanning.html.twig',array('orgas'=>$orgas,'taches'=>$taches,'debut'=>$debut));
+
+            return new Response($pdfGenerator->generatePDF($html),
                 200,
                 array(
                     'Content-Type' => 'application/pdf',
