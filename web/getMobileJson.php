d7651bcb0eaec766d6d9d942ab2add5bd1f18562
Léo Martinez <leo84.martinez@gmail.com>
2015-03-15 12:42:52 +0100

    Bugfixes & fixes security issue

--- web/getMobileJson.php
+++ web/getMobileJson.php
@@ -1,25 +1,25 @@
 <?php
-	$files = scandir('up/jsonMobile', SCANDIR_SORT_DESCENDING);
-	$newestFile = $files[0];
+$files = scandir('up/jsonMobile', SCANDIR_SORT_DESCENDING);
+$newestFile = $files[0];
 
-	if ($__GET['version'] && $__GET['version'] == intval($__GET['version']))
-	{
-		// Check version
-		$latestVersion = substr($newestFile, '7', 14);
-		
-		if ($__GET['version'] != $latestVersion)
-		{
-			header('Content-Type: application/json');
-			echo file_get_contents('up/jsonMobile/' . $newestFile);
-		}
-		else
-		{
-			echo 'nothing new';
-		}
-	}
-	else
-	{
-		header('Content-Type: application/json');
-		echo file_get_contents('up/jsonMobile/' . $newestFile);
-	}
+if ($_GET['version'] && $_GET['version'] == intval($_GET['version']))
+{
+// Check version
+    $latestVersion = substr($newestFile, '7', 14);
+
+    if ($_GET['version'] != $latestVersion)
+    {
+        header('Content-Type: application/json');
+        echo file_get_contents('up/jsonMobile/' . $newestFile);
+    }
+    else
+    {
+        echo 'nothing new';
+    }
+}
+else
+{
+    header('Content-Type: application/json');
+    echo file_get_contents('up/jsonMobile/' . $newestFile);
+}
 ?>
